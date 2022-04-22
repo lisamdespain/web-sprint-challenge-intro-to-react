9af101fa61a8f18d331a81395c2d72d2ede63ec9
Lisa DeSpain <lisamdespain@gmail.com>
2022-04-22 12:24:59 -0500

    now to make pretty

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
-import React from 'react';
-
+import React, { useState, useEffect } from 'react';
+import Character from './components/Character';
+import axios from 'axios';
+import styled from "styled-components";
+import { data } from "./mocks/handlers"
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
@@ -7,12 +10,29 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  const [characters, setCharacters] = useState([]);
 
-  return (
-    <div className="App">
-      <h1 className="Header">Characters</h1>
-    </div>
-  );
-}
-
-export default App;
+  useEffect(() =>{
+    axios.get(`https://swapi.dev/api/people/`)
+    .then(res =>{
+      setCharacters(res.data);
+     
+    })
+    .catch(err => console.log(err))
+  }, [])
+  
+    return (
+      <div className="App">
+        <h1 className="Header">Characters</h1>
+        {
+          characters.map(char =>{
+            return <Character key={[]} characterName={char.name} birthYear={char.birth_year} />
+          })
+        }
+                  
+        
+      </div>
+    );
+  }
+  
+  export default App;
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,22 @@
+import React from 'react';
+import styled, {keyframes} from "styled-components";
+
+
 // Write your Character component here
+
+const Character = props => {
+    // Try to think through what state you'll need for this app before starting. Then build out
+    // the state properties here.
+  const {uuid, characterName, birthYear} = props;
+    // Fetch characters from the API in an effect hook. Remember, anytime you have a 
+    // side effect in a component, you want to think about which state and/or props it should
+    // sync up with, if any.
+    return (
+      <div className="characterInfo">
+        <p className="characterName">{characterName}</p>
+        <p className="birthYear">{birthYear}</p>
+      </div>
+  );
+  }
+  
+  export default Character;
